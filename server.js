f08a2aba48c1e5c76ab469b7d8a9b06b22611f35
Gino Filicetti <gino@filicetti.net>
2016-05-31 12:46:52 -0400

    adding server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var port = process.env.PORT || 8000; // first change
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  //console.log('listening on *:3000');
+});
